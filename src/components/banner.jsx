e0e954b5e470f152ce2b4146fccc8189bb4e50ca
Lagzen Thakuri <thakurizen2@gmail.com>
2024-01-11 17:32:01 +0545

    Added a horizontal rule to the banner component and an image to the projects component.

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -59,6 +59,7 @@ export default function Banner(){
                 </div>
             </div>
         </div>
+                <hr />
     
     </>
 }
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,9 +1,13 @@
  export default function Projects(){
     return <>
     <div>
-       <h1 className="justify-center flex font-bold font-mono text-3xl">Projects </h1>
+       <h1 className="justify-center flex font-bold font-mono text-3xl m-2">Projects </h1>
        <div>
-        <p><a href="https://luckyzen69.github.io/Dark-complex/#home">Dark Complex</a></p>
+
+        <div>
+            <img src='../assets/starmall.jpg' alt="image" />
+            <a href="https://luckyzen69.github.io/Dark-complex/#home">Dark Complex</a>
+            </div>
        </div>
     </div>
         
